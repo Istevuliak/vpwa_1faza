9657e6b3716f81a9c485901ea7d43c0f8127ea56
TereziaKliestikova <xklestikova@stuba.sk>
2025-10-07 14:30:09 +0200

    skuska quasar komponentov

--- src/main.js
+++ src/main.js
@@ -3,4 +3,16 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 💎 Import Quasaru
+import { Quasar } from 'quasar'
+import 'quasar/src/css/index.sass' // alebo 'quasar/dist/quasar.css'
+
+// vytvor aplikáciu
+const app = createApp(App)
+
+// použijeme Quasar ako plugin
+app.use(Quasar, {
+  plugins: {} // sem sa dajú doplniť Quasar pluginy neskôr (napr. Notify)
+})
+
+app.mount('#app')
