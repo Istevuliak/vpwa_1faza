f8ffd9d91645c184f4fed05d57b975ff06ed72a0
TereziaKliestikova <xklestikova@stuba.sk>
2025-10-14 13:24:52 +0200

    logout

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -10,7 +10,7 @@
         </q-toolbar-title>
 
         <!-- Ikony vpravo -->
-        <div class="row items-center q-gutter-sm">
+        <div v-if="isChatPage" class="row items-center q-gutter-sm">
           <!-- Ikona používateľa (otvára pravé menu) -->
           <q-btn
             flat
@@ -23,10 +23,24 @@
           </q-btn>
 
           <!-- Log out -->
-          <q-btn color="dark" flat @click="logout">
+          <q-btn color="dark" flat>
             <q-icon class="material-symbols-outlined q-mr-xs">logout</q-icon>
             Log out
+
+            <!-- menu as slot -->
+            <q-menu v-model="showLogoutMenu" anchor="bottom right" self="top right" :offset="[0, 16]">
+              <q-card style="min-width: 200px;">
+                <q-card-section class="text-center">
+                  Do you want to log out?
+                </q-card-section>
+                <q-card-actions align="right">
+                  <q-btn flat label="Cancel" v-close-popup @click="showLogoutMenu = false" />
+                  <q-btn color="primary" label="Log out" @click="confirmLogout" />
+                </q-card-actions>
+              </q-card>
+            </q-menu>
           </q-btn>
+
         </div>
       </q-toolbar>
     </q-header>
@@ -58,9 +72,13 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+// zistenie kde sa nachadzame
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const isChatPage = computed(() => route.path === '/chat'); 
 
 const router = useRouter();
 
@@ -72,10 +90,14 @@ const linksList: EssentialLinkProps[] = [
 
 const rightDrawerOpen = ref(false);
 
-const logout = async () => {
-  alert('Log out successful!');
+// logout overlay
+const showLogoutMenu = ref(false);
+
+async function confirmLogout() {
+  showLogoutMenu.value = false;
   await router.push('/login');
-};
+}
+
 
 function toggleRightDrawer() {
   rightDrawerOpen.value = !rightDrawerOpen.value;
